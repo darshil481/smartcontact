a3f0a1218310b954675333088c94889c11c0883d
darshil481 <darshil.parmar.23.esparkbiz@gmail.com>
2023-05-18 15:44:54 +0530

    show contatc

--- src/main/java/com/example/smartContact/repository/contactRepository.java
+++ src/main/java/com/example/smartContact/repository/contactRepository.java
@@ -3,7 +3,12 @@ package com.example.smartContact.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.smartContact.model.contact;
 import org.springframework.stereotype.Repository;
+import com.example.smartContact.model.user;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface contactRepository extends JpaRepository<contact,Integer> {
+    public List<contact> findByUser(Optional<user> user);
 }
