7e44dfecf0ef90334019509d608c90d12fe957e2
darshil481 <darshil.parmar.23.esparkbiz@gmail.com>
2023-05-24 16:48:29 +0530

    serching and pagination

--- src/main/java/com/example/smartContact/controller/showContactController.java
+++ src/main/java/com/example/smartContact/controller/showContactController.java
@@ -1,17 +1,19 @@
 package com.example.smartContact.controller;
 
+import com.example.smartContact.model.user;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.smartContact.model.contact;
 import com.example.smartContact.repository.userRepository;
 import com.example.smartContact.repository.contactRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 
@@ -20,11 +22,21 @@ public class showContactController {
     private userRepository userRepository;
     @Autowired
     private contactRepository contactRepository;
-    @GetMapping("/showcontact")
-    public List<contact> solve(HttpSession session){
-       // int id=Integer.valueOf(session.getAttribute("id").toString());
-        List<contact> list=contactRepository.findByUser(userRepository.findById(1));
-        return list;
-    }
 
+
+
+    @GetMapping("/showcontact")
+    public void solve(HttpSession session, @RequestParam(defaultValue = "0") Integer pageNo,
+                      @RequestParam(defaultValue = "5") Integer pageSize,@RequestParam(value = "search",required = false ) String search){
+
+        Optional<user> user = userRepository.findById(1);
+        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        if(search == null){
+            Page<contact> contacts = contactRepository.findByuser(user.get(), pageable);
+        }else{
+            Page<contact> contacts = contactRepository.findBySpecificuser(1, pageable,search);
+
+        }
+
+    }
 }
--- src/main/java/com/example/smartContact/repository/userRepository.java
+++ src/main/java/com/example/smartContact/repository/userRepository.java
@@ -2,9 +2,11 @@ package com.example.smartContact.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.smartContact.model.user;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface userRepository extends JpaRepository<user,Integer> {
+
     user findByEmail(String email);
 }
